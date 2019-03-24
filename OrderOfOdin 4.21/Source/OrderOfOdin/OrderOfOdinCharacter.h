f48ea24ed9b3ed7ecf9de21dcf89647ec74204a3
Stefan <36249159+thecubist@users.noreply.github.com>
2019-03-24 12:49:04 +0000

    began work on AI systems

--- OrderOfOdin 4.21/Source/OrderOfOdin/OrderOfOdinCharacter.h
+++ OrderOfOdin 4.21/Source/OrderOfOdin/OrderOfOdinCharacter.h
@@ -11,13 +11,13 @@ class AOrderOfOdinCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+	///** Camera boom positioning the camera behind the character */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//class USpringArmComponent* CameraBoom;
 
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
+	///** Follow camera */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//class UCameraComponent* FollowCamera;
 public:
 	AOrderOfOdinCharacter();
 
